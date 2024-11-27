8b3a97f662d06c9535c7ecfd59f45b4f635eec82
Arturo Sánchez <danielarturosanchezgodoy@gmail.com>
2024-11-27 19:15:13 +0000

    Fin de actividad

--- DEW/ut04-01/js/main.js
+++ DEW/ut04-01/js/main.js
@@ -1,10 +1,24 @@
 
 const dniNie = document.querySelector("#dni-nie");
 const dniNieText = document.querySelector("#dni--nie");
+const titulo = document.querySelector("#titulo");
+const description = document.querySelector("#description");
+const titleLenght = document.querySelector("#title-length");
+const descriptionLength = document.querySelector("#description-length");
 dniNieText.setAttribute('disabled','true');
+const password = document.querySelector('#contrasenia');
+const showPassword = document.querySelector("#mostrar-contrasenia");
 
 dniNie.addEventListener("change", () => changeState()
 );
+//titulo
+titulo.addEventListener("change", ()=>updateCounter(titulo,titleLenght) );
+titulo.addEventListener("keyup", ()=>updateCounter(titulo, titleLenght) );
+//description
+description.addEventListener("change", ()=>updateCounter(description,descriptionLength) );
+description.addEventListener("keyup", ()=>updateCounter(description, descriptionLength) );
+//mostrar contraseña
+showPassword.addEventListener('change', ()=> showMyPassword())
 
 function changeState() {
     if (dniNie.selectedOptions[0].value !== "") {
@@ -14,10 +28,16 @@ function changeState() {
       }
   }
 
-function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  }
+function showMyPassword(){
+  if(showPassword.checked){
+    password.type="text"
+  } else {
+    password.type="password"
+  } 
+} 
+
+function updateCounter(element, textElement){
+  let textElementInfo = element.value.split("").length
+  textElementInfo = `${textElementInfo}/${element.maxLength}`
+  textElement.textContent = textElementInfo;
+}
