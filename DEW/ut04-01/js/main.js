73e8dba5d649316eb508ed564981f5817975b6ee
2dawa <2dawa@informatica.edu>
2024-11-28 13:11:07 +0000

    Edit select birth year

--- DEW/ut04-01/js/main.js
+++ DEW/ut04-01/js/main.js
@@ -7,6 +7,19 @@ const titleLenght = document.querySelector("#title-length");
 const descriptionLength = document.querySelector("#description-length");
 const password = document.querySelector('#contrasenia');
 const showPassword = document.querySelector("#mostrar-contrasenia");
+const anioNacimiento = document.querySelector("#birth-year");
+
+(function() {
+    let anioMaximo = new Date(Date.now());
+    console.log(anioMaximo.getFullYear());
+
+    for(let i=anioMaximo.getFullYear(); i > 1900; i--){
+      let optionElement = document.createElement("option");
+      optionElement.value=i;
+      optionElement.textContent=i;
+      anioNacimiento.appendChild(optionElement);
+    }
+  })();
 
 dniNieText.setAttribute('disabled','true');
 
@@ -38,7 +51,7 @@ function showMyPassword(){
 } 
 
 function updateCounter(element, textElement){
-  let textElementInfo = element.value.split("").length
+  let textElementInfo = element.value.length
   textElementInfo = `${textElementInfo}/${element.maxLength}`
   textElement.textContent = textElementInfo;
 }
